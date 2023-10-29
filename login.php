f8afbb1683ceaf8ac981d23a9878f20d6f88c13a
kathleenforgiarini <kath.for199@gmail.com>
2023-10-28 23:42:09 -0400

    adding login authentication

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: Get, Post");
+    header("Access-Control-Allow-Headers: Content-Type");
+    
+    $connection = new mysqli("127.0.0.1", "root", "", "dbpem");
+    if (mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit();
+    } else {
+        $eData = file_get_contents("php://input");
+        $dData = json_decode($eData, true);
+        
+        $email = $dData['email'];
+        $pass = $dData['pass'];
+        $result = "";
+        
+        if ($email != "" and $pass != ""){
+            $sql = "SELECT * FROM user WHERE email = '$email'";
+            $res = mysqli_query($connection, $sql);
+            if (mysqli_num_rows($res) != 0){
+                $row = mysqli_fetch_array($res);
+                if($pass != $row['password']){
+                    $result = "Invalid password!";
+                }
+                else {
+                    $result = "Loggedin successfully! Redirecting...";
+                }
+               
+            } else {
+                $result = "Invalid email!";
+            }
+        } else {
+            $result = "";
+        }
+        
+        $connection -> close();
+        $response[] = array("result" => $result);
+        echo json_encode($response);
+    }
+?>
